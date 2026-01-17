a00958d5114d86f3ce7105e3f4654f360e6ac0c6
Bartlomiej-Bogusz <bartlomiej.bogusz1@gmail.com>
2026-01-17 14:28:12 +0100

    Add /metrics to main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import os
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from app.routers import books
 from app.database import Base, engine
 from contextlib import asynccontextmanager
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 Base.metadata.create_all(bind=engine)
 
@@ -25,9 +26,24 @@ app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 app.include_router(books.router)
 
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total number of HTTP requests"
+)
+
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.middleware("http")
+async def count_requests(request, call_next):
+    response = await call_next(request)
+    REQUEST_COUNT.inc()
+    return response
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 
 if __name__ == "__main__":
